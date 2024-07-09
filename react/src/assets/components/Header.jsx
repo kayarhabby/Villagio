92f2ae8185fbed096d65d7c90597d567e1fc7ca4
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2024-07-10 01:00:19 +0200

    création du components Header

---
+++ react/src/assets/components/Header.jsx
@@ -0,0 +1,20 @@
+export default function Header() {
+    return (
+        <article>
+            <a href="">
+                Le Marche
+            </a>
+            <h1>Villa Bastilicata Grande</h1>
+            <p>
+                This is a perfect villa with spa center and hot tub for private, family and corporate rest in Le Marche
+                region in Italy, with best nature views.
+            </p>
+            <p>
+                Prices start at: $630 for 3 nights (+taxes and fees)
+            </p>
+            <a href="">
+                <button>BOOK</button>
+            </a>
+        </article>
+    )
+}
