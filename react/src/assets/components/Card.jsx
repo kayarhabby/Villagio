18a4110f8a4b8994df8c7e2f3d05f77c425c476a
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2024-07-10 00:58:35 +0200

    création de la card qui pourrait utilisé pour plusieurs produits

---
+++ react/src/assets/components/Card.jsx
@@ -0,0 +1,27 @@
+export default function Card() {
+    return (
+        <article>
+            <img src="" alt=""/>
+            <h2> Title </h2>
+            <p>
+                Le Marche Etna House is a fully equipped villa with many 3 big rooms, swimming pool, terrace
+                and lots of other facilities.
+            </p>
+            <div>
+
+            </div>
+            <div>
+                Amenities: Air conditioning, Balcony, Beachfront, Dining area, Flat-screen TV, Free parking,
+                Free WiFi, Outdoor poolView: Swimming pool Bed Type: 1 Queen bad, 4 Tween beds, 1 Full bed
+                Categories: Le Marche
+            </div>
+            <div>
+                Prices start at: $750 for 3 nights (+taxes and fees)
+            </div>
+            <div>
+                <button>BOOK</button>
+                <a href="">View Details</a>
+            </div>
+        </article>
+    )
+}
