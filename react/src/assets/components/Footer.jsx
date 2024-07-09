b799039117b692f43ce7c805739de89d9883dff4
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2024-07-10 00:59:25 +0200

    création du components footer

---
+++ react/src/assets/components/Footer.jsx
@@ -0,0 +1,43 @@
+export default function Footer() {
+    return (
+        <footer>
+            <article>
+                <h2>ABOUT US</h2>
+                <p>
+                    We are one of the most recognized happy vacation makers in Italy – we provide a wide range of
+                    great villas for any occasion since 2015. Any type of a winter or summer villa can be found on
+                    the site.
+                </p>
+            </article>
+
+            <article>
+                <h2>MULTILINGUAL VILLAGIO</h2>
+                <small>
+                    The demo content in German is not a professional translation! It’s sample data used to
+                    demonstrate a multilingual website (localization was performed via WPML plugin, which is NOT
+                    included into the theme).
+
+                    Note: multi-currency is performed via addon, which is NOT included into the theme.
+                </small>
+            </article>
+
+            <article>
+                <h2>RECENT NEWS</h2>
+                <a href="">Unforgettable Vignanello garden tour in Lazio</a>
+                <a href="">Special discounts for Italian castles tour!</a>
+                <a href="">Places of interest neat summer villa Emilia</a>
+                <a href="">Celebrate Lucca Summer Festival with us</a>
+                <a href="">Lazio or Campania: which location is best for your stay?</a>
+            </article>
+
+            <article>
+                <h2>Links</h2>
+                <a href="/">How to book</a>
+                <a href="/">General letting conditions</a>
+                <a href="/">JObs and careers</a>
+                <a href="/">Travel agent resources</a>
+                <a href="/">Contact us</a>
+            </article>
+        </footer>
+    )
+}
