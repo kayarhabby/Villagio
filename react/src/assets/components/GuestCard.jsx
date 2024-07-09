4076bfdee2ad05ed1c6479c8aac23b118c3492f2
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2024-07-10 00:59:44 +0200

    création du components GuestCard

---
+++ react/src/assets/components/GuestCard.jsx
@@ -0,0 +1,14 @@
+export default function GuestCard() {
+    return (
+        <div className="guest-content">
+            <div>
+                <img src="" alt=""/>
+            </div>
+            <p>
+                I was in Italy for the first time and stayed by chance in a winder cottage Monte Bianco.
+                I must say that was an unforgettable experience!
+            </p>
+            <a href=""> JOE DOE</a>
+        </div>
+    )
+}
