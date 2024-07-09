23ca46b7b003aa55e98d981f6c2663fdfbdd627b
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2024-07-10 01:06:34 +0200

    déplacement des fichiers css index et App dans src/css

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './assets/css/index.css'
 import {RouterProvider} from "react-router-dom";
 import router from "./assets/routes/router.jsx";
 
