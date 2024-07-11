5d14a4635bf99243e5b4e05c9b7d988488eb2a6b
Doumani <doumanisamuel@gmail.com>
2024-07-11 15:30:45 +0000

    Configuration complète de LieuController

--- app/Http/Controllers/LieuController.php
+++ app/Http/Controllers/LieuController.php
@@ -2,12 +2,70 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lieu;
 use Illuminate\Http\Request;
+use App\Http\Resources\LieuResource;
 
 class LieuController extends Controller
 {
+    //Permet de récupérer toutes les ressources
     public function index()
     {
-        return 'Bonjour la famille';
+        return LieuResource::collection(Lieu::all());
     }
+
+    //Permet de récupérer une ressource
+    public function show(Lieu $lieu)
+    {
+        return new LieuResource($lieu);
+    }
+
+    //Création d'une ressource
+    public function store(Request $request)
+    {
+        //Validation des données
+        $request->validate([
+            'Nom'=>'required|string|max:50',
+        ]);
+
+        //Création de la ressource dans la base de données
+        $lieu = Lieu::create([
+            'Nom'=>$request->Nom,
+        ]);
+
+        //Retourner la ressource créée dans LieuResource
+        return new LieuResource($lieu);
+    }
+
+    //Modification d'une ressource
+    public function update(Request $request, Lieu $lieu)
+    {
+        //Validation des données
+        $request->validate([
+            'Nom'=>'sometimes|required|string|max:50',
+        ]);
+
+        //Mis à jour de la ressource dans la BDD
+        if($request->has('Nom')){
+            $lieu->Nom = $request->Nom;
+        }
+
+        //Enregistrement de la ressource
+        $lieu->save();
+
+        //Retourner la ressource créée dans LieuResource
+        return new LieuResource($lieu);
+
+    }
+
+    //Suppresion d'une resource
+    public function destroy(Lieu $lieu)
+    {
+        $lieu->delete();
+
+        //Retourner une réponse vide avec un statut 204 No content
+        return response()->json(null, 204);
+    }
+
+
 }
