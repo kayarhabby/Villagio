2800454c8ca50eb884f551f324c6f669791ce986
Doumani <doumanisamuel@gmail.com>
2024-07-11 15:58:34 +0000

    Co

--- app/Http/Resources/VillaResource.php
+++ app/Http/Resources/VillaResource.php
@@ -14,6 +14,21 @@ class VillaResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id_villa'=>$this->id_villa,
+            'id_lieu'=>$this->id_lieu,
+            'Titre'=>$this->Titre,
+            'Description'=>$this->Description,
+            'Image'=>$this->Image,
+            'Adultes'=>$this->Adultes,
+            'Enfants'=>$this->Enfants,
+            'Equipements'=>$this->Equipements,
+            'Vue'=>$this->Vue,
+            'Superficie'=>$this->Superficie,
+            'Prix'=>$this->Prix,
+            'Statut'=>$this->Statut,
+            'Date_debut_disponible'=>$this->Date_debut_disponible,
+            'Date_fin_disponible'=>$this->Date_fin_disponible,
+        ];
     }
 }
