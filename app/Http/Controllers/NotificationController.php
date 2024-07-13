5059f47e45afcceaf815bf110587e264add91804
Doumani <doumanisamuel@gmail.com>
2024-07-13 13:55:47 +0000

    Configuration du controleur notification

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -2,12 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Notification;
 use Illuminate\Http\Request;
+use App\Http\Resources\NotificationResource;
 
 class NotificationController extends Controller
 {
+    //Récupérer toutes les ressources
     public function index()
     {
-        return 'Bonjour la famille';
+        return NotificationResource::collection(Notification::all());
+    }
+
+    //Récupérer une seule ressource
+    public function show(Notification $notification)
+    {
+        return new NotificationResource($notification);
+    }
+
+    //Créer une nouvelle notification
+    public function store(Request $request)
+    {
+        //Validation des données
+        $request->validate([
+            'id_client'=>'required|exists:client,id_client',
+            'Sujet'=>'required|text',
+            'Message'=>'required|text',
+        ]);
+
+        //Création d'une nouvelle notification dans la BDD
+        $notification = Notification::create([
+            'id_client'=>$request->id_client,
+            'Sujet'=>$request->Sujet,
+            'Message'=>$request->Message,
+        ]);
+        
+        //Retourner la ressource créée
+        return new NotificationResource($notification);
+    }
+
+    //Supprimer une ressource
+    public function destroy(Notification $notification)
+    {
+        $notification->delete();
+
+        //Retourner une réponse vide avec un statut 204 No content
+        return response()->json(null, 204); 
     }
 }
