f464d58b9d5344d634af543ab322dfa9a9fd32cf
Doumani <doumanisamuel@gmail.com>
2024-07-13 13:39:22 +0000

    Configuration de la ressource notification

--- app/Http/Resources/NotificationResource.php
+++ app/Http/Resources/NotificationResource.php
@@ -14,6 +14,11 @@ class NotificationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id_notification'=>$this->id_notification,
+            'id_client'=>$this->id_client,
+            'Sujet'=>$this->Sujet,
+            'Message'=>$this->Message,
+        ];
     }
 }
