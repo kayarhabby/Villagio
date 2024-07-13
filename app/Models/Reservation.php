8c71530b554be19f8114d087b6fd7cd8188faaaf
Doumani <doumanisamuel@gmail.com>
2024-07-13 12:34:27 +0000

    Modification du modèle reservation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -7,7 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
+    protected $table = 'reservation';//Spécifier le nom de la table
     protected $primaryKey = 'id_reservation';
+    public $incrementing = true;//Activer l'auto-incrémentation
+
+    protected $fillable = [
+        'id_villa',
+        'id_client',
+        'Date_debut_reservation',
+        'Date_fin_reservation',
+        'Statut',
+    ];
 
     //Une reservation appartient à un client
     public function client()
