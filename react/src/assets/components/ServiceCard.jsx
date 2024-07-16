072602273417a1db6f87fc076c8b09696eb44f0b
kayarhabby <pasciahershekaya@gmail.com>
2024-07-16 16:26:51 +0200

    changement mineur

---
+++ react/src/assets/components/ServiceCard.jsx
@@ -0,0 +1,29 @@
+export default function ServiceCard() {
+    return (
+        <article>
+            <img src="" alt=""/>
+            <section>
+                <h2> Title </h2>
+                <p>
+                    Take children with you? Let them have the best vacation and let yourself! Have a free time while our
+                    baby sitters take care of your children – we have a lot of interesting things to do – playing,
+                    swimming, cooking, etc.
+
+                    In our villas it is possible to request the services of a qualified babysitter who can look after
+                    your children on those tours, excursions, which are not suitable for them, or simply for a romantic
+                    evening.
+
+                    The service is agreed upon according to your needs, contact us for availability. The price listed is
+                    a deposit that we’ll add to the final service price.
+                </p>
+            </section>
+
+            <section>
+                <h2> Title </h2>
+                <p>
+                    $23 / Per Day / Per Guest
+                </p>
+            </section>
+        </article>
+    )
+}
