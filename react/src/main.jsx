e5cd2e48fd1121e15a86193fedec6a5bd22ef2d5
kayarhabby <pasciahershekaya@gmail.com>
2024-07-16 18:17:48 +0200

    insertion template css

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/css/index.css'
+import './assets/css/template.css'
 import {RouterProvider} from "react-router-dom";
 import router from "./assets/routes/router.jsx";
 
