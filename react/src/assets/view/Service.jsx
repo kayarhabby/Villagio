7a9996cce2eac201e72f32e4cadceee12cc7ee87
kayarhabby <pasciahershekaya@gmail.com>
2024-07-16 18:29:15 +0200

    mise en place de la page service

--- react/src/assets/view/Service.jsx
+++ react/src/assets/view/Service.jsx
@@ -1,36 +1,30 @@
 import ServiceCard from "../components/ServiceCard.jsx";
-import FormHome from "../components/HOME/FormHome.jsx";
-import Header from "../components/HOME/Header.jsx";
 import Footer from "../components/HOME/Footer.jsx";
 import Navbar from "../components/HOME/Navbar.jsx";
+import Aside from "../components/Aside.jsx";
 
 export default function Service() {
     return (
         <div id="service">
             <Navbar />
             <main>
-                <ServiceCard />
-                <aside>
-                    <FormHome />
-                    <section>
-                        <h3>Région</h3>
-                        <div>
-                            <button>Aosta Valley</button>
-                            <button>Lazio</button>
-                            <button>Le Marche</button>
-                        </div>
-                    </section>
-
-                    <section>
-                        <h3>WE RECOMMEND</h3>
-                        <img src="" alt=""/>
-                        <h4>Summer VIlla Emilia</h4>
-                        <p> Prices start at: $561 for 3 nights (+taxes and fees)</p>
-                        <button>BOOK</button>
-                    </section>
-                </aside>
+                <section>
+                <div>
+                    <h1>Archives: Service</h1>
+                    <ServiceCard/>
+                </div>
+                <div>
+                    <h1>Archives: Service</h1>
+                    <ServiceCard/>
+                </div>
+                <div>
+                    <h1>Archives: Service</h1>
+                    <ServiceCard/>
+                </div>
+                </section>
+                <Aside/>
             </main>
-            <Footer />
+            <Footer/>
         </div>
     )
 }
