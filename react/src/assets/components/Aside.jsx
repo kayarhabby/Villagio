92fcab7e4e32a38bff1ee2423b2b483e33f05954
kayarhabby <pasciahershekaya@gmail.com>
2024-07-18 14:53:15 +0200

    ajout de section searchbar et recents comments

--- react/src/assets/components/Aside.jsx
+++ react/src/assets/components/Aside.jsx
@@ -1,4 +1,5 @@
 import FormHome from "./HOME/FormHome.jsx";
+import Lemarche from '/src/assets/images/Le_marche_Etna_House.jpg';
 
 export default function () {
     return (
@@ -6,7 +7,7 @@ export default function () {
             <FormHome/>
             <section>
                 <h3>Région</h3>
-                <div>
+                <div className="regionbtn">
                     <button>Aosta Valley</button>
                     <button>Lazio</button>
                     <button>Le Marche</button>
@@ -15,10 +16,41 @@ export default function () {
 
             <section>
                 <h3>WE RECOMMEND</h3>
-                <img src="" alt=""/>
+                <img src={Lemarche} alt=""/>
                 <h4>Summer VIlla Emilia</h4>
                 <p> Prices start at: $561 for 3 nights (+taxes and fees)</p>
-                <button>BOOK</button>
+                <button className="btn">BOOK</button>
+            </section>
+
+            <section>
+                <article className="footer-link">
+                    <h3>RECENT COMMENTS</h3>
+                    <div>
+                        <a href="">Unforgettable Vignanello garden tour in Lazio</a>
+                    </div>
+                    <div>
+                        <a href="">Special discounts for Italian castles tour!</a>
+                    </div>
+                    <div>
+                        <a href="">Places of interest neat summer villa Emilia</a>
+                    </div>
+                    <div>
+                        <a href="">Celebrate Lucca Summer Festival with us</a>
+                    </div>
+                    <div>
+                        <a href="">Lazio or Campania: which location is best for your stay?</a>
+                    </div>
+                </article>
+            </section>
+
+            <section>
+                <h3>Search</h3>
+                <div className="search-container">
+                    <form action="">
+                        <input type="text" placeholder="Search.." name="search"/>
+                        {/*<button type="submit"><i className="fa fa-search" aria-hidden="true"></i></button>*/}
+                    </form>
+                </div>
             </section>
         </aside>
     )
