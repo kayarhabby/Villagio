1fa548237e9df0d45608334b19fd4ccef09bab1b
kayarhabby <pasciahershekaya@gmail.com>
2024-07-19 20:12:52 +0200

    mis en place de la structure avant apport des villas de la bd

--- react/src/assets/view/Regions/AostaValley.jsx
+++ react/src/assets/view/Regions/AostaValley.jsx
@@ -1,18 +1,20 @@
 import RegionTemplate from "./RegionTemplate.jsx";
 import Navbar from "../../components/HOME/Navbar.jsx";
-import ServiceCard from "../../components/ServiceCard.jsx";
 import Aside from "../../components/Aside.jsx";
 import Footer from "../../components/HOME/Footer.jsx";
 import React from "react";
 
 export default function () {
+
+    const villas = {};
+
     return (
         <div className="template">
             <Navbar/>
             <main>
-                <section>
-                    <RegionTemplate/>
-                </section>
+                {villas.map((villa) => (
+                    <RegionTemplate key={villa.id} {...villa}/>
+                ))}
                 <Aside/>
             </main>
             <Footer/>
