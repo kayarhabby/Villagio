dd7ae5bd723c26385a1db992fdc73b83931db7c5
kayarhabby <pasciahershekaya@gmail.com>
2024-07-19 14:40:15 +0200

    ajout des routes

--- react/src/assets/routes/router.jsx
+++ react/src/assets/routes/router.jsx
@@ -2,6 +2,12 @@ import {createBrowserRouter} from "react-router-dom";
 import Home from "../view/Home.jsx";
 import Service from "../view/Service.jsx";
 import News from "../view/News.jsx";
+import Activities from "../view/Activities.jsx";
+import AostaValley from "../view/Regions/AostaValley.jsx";
+import Lazio from "../view/Regions/Lazio.jsx";
+import LeMarche from "../view/Regions/LeMarche.jsx";
+import AllVillas from "../view/Regions/AllVillas.jsx";
+import FindYourVilla from "../view/Regions/FindYourVilla.jsx";
 
 
 const router = createBrowserRouter([
@@ -11,13 +17,51 @@ const router = createBrowserRouter([
         element : <Home />
     },
     {
+        path : '/aostaValley',
+        element : <AostaValley />
+    },
+    {
+        path : '/lazio',
+        element : <Lazio />
+    },
+    {
+        path : '/leMarche',
+        element : <LeMarche />
+    },
+    {
+        path : '/allVillas',
+        element : <AllVillas />
+    },
+    {
+        path : '/findYourVilla',
+        element : <FindYourVilla />
+    },
+
+    {
         path : '/service',
         element : <Service />
     },
     {
         path : '/news',
         element : <News />
-    }
+    },
+    {
+        path : '/activities',
+        element : <Activities />,
+    },
+    {
+        path : '/CapriBoatTourFromSorrento',
+        element : <Service />
+    },
+    {
+        path : '/SightseeingNaples',
+        element : <Service />
+    },
+    {
+        path : '/LearnHistory',
+        element : <Service />
+    },
+
 ])
 
 export default router;
