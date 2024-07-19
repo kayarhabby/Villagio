674a9214e6902fcf3ebc66036b0a42f85d8f2b24
kayarhabby <pasciahershekaya@gmail.com>
2024-07-19 20:15:20 +0200

    mis en place de la structure avant apport des villas de la bd

--- react/src/assets/view/Regions/LeMarche.jsx
+++ react/src/assets/view/Regions/LeMarche.jsx
@@ -1,7 +1,22 @@
-export default function () {
-    return (
-        <>
+import Navbar from "../../components/HOME/Navbar.jsx";
+import RegionTemplate from "./RegionTemplate.jsx";
+import Aside from "../../components/Aside.jsx";
+import Footer from "../../components/HOME/Footer.jsx";
+import React from "react";
 
-        </>
+export default function () {
+    const villas = {};
+
+    return (
+        <div className="template">
+            <Navbar/>
+            <main>
+                {villas.map((villa) => (
+                    <RegionTemplate key={villa.id} {...villa}/>
+                ))}
+                <Aside/>
+            </main>
+            <Footer/>
+        </div>
     )
 }
