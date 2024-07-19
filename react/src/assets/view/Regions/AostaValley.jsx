adce7dbb4cc0b99fc857a13323fc8c279da7a20e
kayarhabby <pasciahershekaya@gmail.com>
2024-07-19 16:49:05 +0200

    premier essaie avec AostaValley

--- react/src/assets/view/Regions/AostaValley.jsx
+++ react/src/assets/view/Regions/AostaValley.jsx
@@ -1,7 +1,21 @@
+import RegionTemplate from "./RegionTemplate.jsx";
+import Navbar from "../../components/HOME/Navbar.jsx";
+import ServiceCard from "../../components/ServiceCard.jsx";
+import Aside from "../../components/Aside.jsx";
+import Footer from "../../components/HOME/Footer.jsx";
+import React from "react";
+
 export default function () {
     return (
-        <>
-
-        </>
+        <div className="template">
+            <Navbar/>
+            <main>
+                <section>
+                    <RegionTemplate/>
+                </section>
+                <Aside/>
+            </main>
+            <Footer/>
+        </div>
     )
 }
