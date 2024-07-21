40627f074351230adbfa39511a6f139c0aadbfa7
kayarhabby <pasciahershekaya@gmail.com>
2024-07-22 00:51:08 +0200

    apports des villas dans les différents regions

--- react/src/assets/view/Regions/Lazio.jsx
+++ react/src/assets/view/Regions/Lazio.jsx
@@ -2,19 +2,46 @@ import Navbar from "../../components/HOME/Navbar.jsx";
 import RegionTemplate from "./RegionTemplate.jsx";
 import Aside from "../../components/Aside.jsx";
 import Footer from "../../components/HOME/Footer.jsx";
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function () {
-    const villas = {};
+    const [villas, setVillas] = useState([]);
+    useEffect(() => {
+        const fetchVillas = async () => {
+            const response = await axios.get(`http://127.0.0.1:8000/api/lieu/2/villa`);
+            setVillas(response.data.data);
+        }
+        fetchVillas();
+    }, []);
 
     return (
         <div className="template">
             <Navbar/>
             <main>
                 <section>
-                    {villas.map((villa) => (
-                        <RegionTemplate key={villa.id} {...villa}/>
-                    ))}
+                    {villas.map((villa) => {
+                        // Split the image URL
+                        const imgPathParts = villa.Image.split('/');
+                        const imgFileName = imgPathParts[imgPathParts.length - 1];
+                        const src = `/src/assets/images/${imgFileName}`;
+
+                        return (
+                            <RegionTemplate
+                                key={villa.id_villa}
+                                src={src}
+                                alt={villa.Titre}
+                                description={villa.Description}
+                                title={villa.Titre}
+                                nbAdult={villa.Adultes}
+                                nbChildren={villa.Enfants}
+                                amenities={villa.Equipements}
+                                size={villa.Superficie}
+                                categories={villa.lieu}
+                                price={villa.Prix}
+                            />
+                        );
+                    })}
                 </section>
                 <Aside/>
             </main>
