04db5e55a1744852e9b175456cc49d9111d8cdd8
kayarhabby <pasciahershekaya@gmail.com>
2024-07-23 00:55:20 +0200

    modification en incluant la navigation entre les pages

---
+++ react/src/assets/view/Testimoniale/Testimonial.jsx
@@ -0,0 +1,65 @@
+// App.js
+import React, { useState } from 'react';
+import {useNavigate} from "react-router-dom";
+
+
+
+const guests = [
+    {
+        id: 1,
+        src: "/src/assets/images/joeDoe.jpg",
+        alt: "JOE DOE",
+        comments: "I was in Italy for the first time and stayed by chance in a winder cottage Monte Bianco. I must say that was an unforgettable experience!",
+        name : "JOE DOE",
+        link: "/testimonial"
+    },
+    {
+        id: 2,
+        src: "/src/assets/images/kimLee.jpg",
+        alt: "KIM LEE",
+        comments: "I’ll never forget our summer stay at villa Emilia! Perfect location, perfect service, amazing nature views. I enjoyed the stay and would recommend it to everyone. Moreover, there are a lot of interesting things to visit and do – fests, food tours, castles tours and much more!",
+        name : "KIM LEE",
+        link: "/testimonial"
+    },
+    {
+        id: 3,
+        src: "/src/assets/images/tomHendrix.jpg",
+        alt: "John Doe",
+        comments: "I was visiting Le Marche with my family and friends and stayed at  Etna House – this was an amazing and unforgettable stay! A lot of space, a pool, a beachfront, just a peaceful rest.",
+        name : "TOM HENDRIX",
+        link: "/testimonial"
+    }
+];
+
+function Testimonial() {
+
+    const navigate = useNavigate();
+
+    const goToTestimonialPage = (index) => {
+        navigate(`/testimonial/${index}`);
+    };
+
+    return (
+        <section id="guest">
+            <h2>
+                Guest Reviews
+            </h2>
+            <div className="guest-container-card">
+                {guests.map((guest, index) => (
+                    <div key={index}>
+                        <article className="guest-container">
+                            <img src={guest.src} alt={guest.alt} className="cercle-img"/>
+                            <section>
+                                <p>{guest.comments}</p>
+                                <button onClick={() => goToTestimonialPage(index)}>En savoir plus</button>
+                            </section>
+                        </article>
+
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default Testimonial;
