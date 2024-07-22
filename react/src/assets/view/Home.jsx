4941f686a37d5e897057620b8abe27b78c68ecff
kayarhabby <pasciahershekaya@gmail.com>
2024-07-22 17:43:54 +0200

    suppression de la grid de testament et remplacement par le component GuestGridCard où est placé le code correspondant

--- react/src/assets/view/Home.jsx
+++ react/src/assets/view/Home.jsx
@@ -2,10 +2,10 @@ import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
 import FormHome from "../components/FormHome.jsx";
 import Card from "../components/Card.jsx";
-import GuestCard from "../components/GuestCard.jsx";
 import Header from "../components/Header.jsx";
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import GuestGridCard from "../components/GuestGridCard.jsx";
 
 export default function Home() {
 
@@ -25,32 +25,7 @@ export default function Home() {
 
     }, []);
 
-    const guests = [
-        {
-            id: 1,
-            src: "/src/assets/images/joeDoe.jpg",
-            alt: "JOE DOE",
-            comments: "I was in Italy for the first time and stayed by chance in a winder cottage Monte Bianco. I must say that was an unforgettable experience!",
-            name : "JOE DOE",
-            link: "/"
-        },
-        {
-            id: 2,
-            src: "/src/assets/images/kimLee.jpg",
-            alt: "KIM LEE",
-            comments: "I’ll never forget our summer stay at villa Emilia! Perfect location, perfect service, amazing nature views. I enjoyed the stay and would recommend it to everyone. Moreover, there are a lot of interesting things to visit and do – fests, food tours, castles tours and much more!",
-            name : "KIM LEE",
-            link: "/"
-        },
-        {
-            id: 3,
-            src: "/src/assets/images/tomHendrix.jpg",
-            alt: "John Doe",
-            comments: "I was visiting Le Marche with my family and friends and stayed at  Etna House – this was an amazing and unforgettable stay! A lot of space, a pool, a beachfront, just a peaceful rest.",
-            name : "TOM HENDRIX",
-            link: "/"
-        }
-    ];
+
 
     return (
         <>
@@ -94,17 +69,7 @@ export default function Home() {
                         );
                     })}
                 </section>
-                <section id="guest">
-                    <h2>
-                        Guest Reviews
-                    </h2>
-
-                    <div className="guest-container-card">
-                        {guests.map((guest) => (
-                            <GuestCard key={guest.id} {...guest} />
-                        ))}
-                    </div>
-                </section>
+                <GuestGridCard />
             </main>
             <Footer />
         </>
