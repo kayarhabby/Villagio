586c8906fc0c95100239ade673b86868ba11059d
kayarhabby <pasciahershekaya@gmail.com>
2024-07-23 00:53:14 +0200

    mise en place de la navigation entre les pages de témoignages

---
+++ react/src/assets/view/Testimoniale/NavigationTestimonial.jsx
@@ -0,0 +1,14 @@
+// components/Navigation.js
+import React from 'react';
+// import './Navigation.css';
+
+function NavigationTestimonial({ currentPage, totalPages, nextPage, prevPage}) {
+    return (
+        <div className="navigation">
+            {currentPage > 0 && <button onClick={prevPage}>Prev</button>}
+            {currentPage < totalPages - 1 && <button onClick={nextPage}>Next</button>}
+        </div>
+    );
+}
+
+export default NavigationTestimonial;
