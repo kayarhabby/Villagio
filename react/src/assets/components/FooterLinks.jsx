4388f6d3eeb93456b8e98d476b296803e8efa0b6
kayarhabby <pasciahershekaya@gmail.com>
2024-07-22 19:08:58 +0200

    component footerLinks mise en place

---
+++ react/src/assets/components/FooterLinks.jsx
@@ -0,0 +1,16 @@
+
+// import React from 'react';
+// import './FooterLinks.css';
+
+function FooterLinks({ title, links }) {
+    return (
+        <article className="footer-link">
+            <h2>{title}</h2>
+            {links.map((link, index) => (
+                <a href={link.href} key={index}>{link.text}</a>
+            ))}
+        </article>
+    );
+}
+
+export default FooterLinks;
