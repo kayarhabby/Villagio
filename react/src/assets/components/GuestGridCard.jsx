a36aa5f2157497b69a193ca7b5b1f083156b6d68
kayarhabby <pasciahershekaya@gmail.com>
2024-07-23 01:00:43 +0200

    inutilisation de la GuestGridCard dans aucune autre des composants de la vue

--- react/src/assets/components/GuestGridCard.jsx
+++ react/src/assets/components/GuestGridCard.jsx
@@ -9,7 +9,7 @@ export default function GuestGridCard() {
             alt: "JOE DOE",
             comments: "I was in Italy for the first time and stayed by chance in a winder cottage Monte Bianco. I must say that was an unforgettable experience!",
             name : "JOE DOE",
-            link: "/"
+            link: "/testimonial"
         },
         {
             id: 2,
@@ -17,7 +17,7 @@ export default function GuestGridCard() {
             alt: "KIM LEE",
             comments: "I’ll never forget our summer stay at villa Emilia! Perfect location, perfect service, amazing nature views. I enjoyed the stay and would recommend it to everyone. Moreover, there are a lot of interesting things to visit and do – fests, food tours, castles tours and much more!",
             name : "KIM LEE",
-            link: "/"
+            link: "/testimonial"
         },
         {
             id: 3,
@@ -25,7 +25,7 @@ export default function GuestGridCard() {
             alt: "John Doe",
             comments: "I was visiting Le Marche with my family and friends and stayed at  Etna House – this was an amazing and unforgettable stay! A lot of space, a pool, a beachfront, just a peaceful rest.",
             name : "TOM HENDRIX",
-            link: "/"
+            link: "/testimonial"
         }
     ];
 
