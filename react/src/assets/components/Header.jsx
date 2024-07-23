57b4c38a46454581aa8ff8087c29c4d0cd97c579
kayarhabby <pasciahershekaya@gmail.com>
2024-07-23 19:50:13 +0200

    modification de la balise Header notamment en supprimant la balise section qui était à l'intérieur

--- react/src/assets/components/Header.jsx
+++ react/src/assets/components/Header.jsx
@@ -1,23 +1,22 @@
+import "../css/Header.css"
 export default function Header() {
     return (
-        <article id="header">
-            <section>
-                <a href="">
-                    Le Marche
-                </a>
-                <h1>Villa Bastilicata Grande</h1>
-                <p>
-                    This is a perfect villa with spa center and hot tub for private, family and corporate rest in Le
-                    Marche
-                    region in Italy, with best nature views.
-                </p>
-                <p>
-                    Prices start at: $630 for 3 nights (+taxes and fees)
-                </p>
-                <a href="">
-                    <button>BOOK</button>
-                </a>
-            </section>
-        </article>
+        <header>
+            <a href="">
+                Le Marche
+            </a>
+            <h1>Villa Bastilicata Grande</h1>
+            <p>
+                This is a perfect villa with spa center and hot tub for private, family and corporate rest in Le
+                Marche
+                region in Italy, with best nature views.
+            </p>
+            <p>
+                Prices start at: $630 for 3 nights (+taxes and fees)
+            </p>
+            <a href="">
+                <button className="btn">BOOK</button>
+            </a>
+        </header>
     )
 }
