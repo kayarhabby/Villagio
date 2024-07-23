4df51491af251ebb078cf729fcc830cfb8948873
kayarhabby <pasciahershekaya@gmail.com>
2024-07-23 21:38:01 +0200

    création du component MainHeaderTextContent afin de contenir le text au niveau de la partie centrale

---
+++ react/src/assets/components/MainHeaderTextContent.jsx
@@ -0,0 +1,18 @@
+import FormHome from "./FormHome.jsx";
+import React from "react";
+
+export default function () {
+    return (
+        <section id="main-header-text-content">
+            <h1>
+                The villa rental expert for your vacation
+            </h1>
+            <p>
+                Villagio offers diverse villas in different regions to help you spend your dream vacation. Book
+                your
+                villa online quickly and safely.
+            </p>
+            <FormHome/>
+        </section>
+    )
+}
