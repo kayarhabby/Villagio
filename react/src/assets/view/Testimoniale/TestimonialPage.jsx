48f069072fae4e958f576e5fe1acd80b97f6a908
kayarhabby <pasciahershekaya@gmail.com>
2024-07-25 15:10:47 +0200

    modification des images

--- react/src/assets/view/Testimoniale/TestimonialPage.jsx
+++ react/src/assets/view/Testimoniale/TestimonialPage.jsx
@@ -11,21 +11,21 @@ import Footer from "../../components/Footer.jsx";
 const guests = [
     {
         id: 1,
-        src: "/src/assets/images/joeDoe.jpg",
+        src: "/src/assets/images/joedoeM.jpg",
         alt: "JOE DOE",
         comments: "I was in Italy for the first time and stayed by chance in a winder cottage Monte Bianco. I must say that was an unforgettable experience!",
         name : "JOE DOE"
     },
     {
         id: 2,
-        src: "/src/assets/images/kimLee.jpg",
+        src: "/src/assets/images/kimLeeM.jpg",
         alt: "KIM LEE",
         comments: "I’ll never forget our summer stay at villa Emilia! Perfect location, perfect service, amazing nature views. I enjoyed the stay and would recommend it to everyone. Moreover, there are a lot of interesting things to visit and do – fests, food tours, castles tours and much more!",
         name : "KIM LEE"
     },
     {
         id: 3,
-        src: "/src/assets/images/tomHendrix.jpg",
+        src: "/src/assets/images/tomHendrixM.jpg",
         alt: "John Doe",
         comments: "I was visiting Le Marche with my family and friends and stayed at  Etna House – this was an amazing and unforgettable stay! A lot of space, a pool, a beachfront, just a peaceful rest.",
         name : "TOM HENDRIX"
