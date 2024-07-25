e8e265d67fa994295429d14f572c6ae483cdf9ed
kayarhabby <pasciahershekaya@gmail.com>
2024-07-25 10:25:41 +0200

    ajout link au niveau des images et navigation au niveau de la page details

--- react/src/assets/components/Card.jsx
+++ react/src/assets/components/Card.jsx
@@ -1,4 +1,4 @@
-
+// Card component to display individual villa details
 export default function Card(props) {
     const {
         src,
@@ -17,21 +17,23 @@ export default function Card(props) {
 
     return (
         <article>
-            <img src={src} alt={alt}/>
-            <h2>{title}</h2>
-            <p className="desc-item-p">{description}</p>
-            <section className="description-item-acs">
+            <a href="#" onClick={(event) => goToVillaDetailsPage(event, title)}><img src={src} alt={alt}/> </a>{/* Villa image*/}
+                <h2>{title}</h2>  {/*Villa title */}
+                <p className="desc-item-p">{description}</p>  {/*Villa description */}
+                <section className="description-item-acs">
                 <span>
                     Adults : {nbAdult} / Children : {nbChildren} / Size : {size} / Amenities : {amenities} / Categories : {categories}.
-                </span>
-            </section>
-            <div className="price-item-acs">
-                <span>Prices start at : </span> <strong>{price}</strong> for 3 nights (+taxes and fees)
-            </div>
-            <div>
-                <button className="btn" onClick={() => goToVillaBookPage(props.title)}>BOOK</button>
-                <button className="btn" onClick={() => goToVillaDetailsPage(props.title)}>View Details</button>
-            </div>
+                </span> {/* Villa details */}
+                </section>
+                <div className="price-item-acs">
+                    <span>Prices start at : </span> <strong>{price}</strong> for 3 nights (+taxes and fees)
+                </div>
+                <div>
+                    <button className="btn" onClick={() => goToVillaBookPage(title)}>BOOK</button>
+                    {/* Booking button */}
+                    <a href="#" onClick={(event) => goToVillaDetailsPage(event, title)}>View
+                        Details</a> {/* Details link */}
+                </div>
         </article>
-    )
+);
 }
