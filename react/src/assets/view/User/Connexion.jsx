2fe0144011c30305e85e821d9a4877f07fd1b4ad
kayarhabby <pasciahershekaya@gmail.com>
2024-07-25 14:01:53 +0200

    ajout de la navbar et du formulaire d'enregistrement

---
+++ react/src/assets/view/User/Connexion.jsx
@@ -0,0 +1,11 @@
+import Navbar from "../../components/Navbar.jsx";
+import RegistrationForm from "./RegistrationForm.jsx";
+
+export default function () {
+    return (
+        <>
+            <Navbar />
+            <RegistrationForm />
+        </>
+    )
+}
