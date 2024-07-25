adecd79c0fe91e4d5ebc863d0a4c4d4ad5be53c3
kayarhabby <pasciahershekaya@gmail.com>
2024-07-25 09:06:39 +0200

    modification de la page VillabookPage, maintenant cette page ne sert que le formulaire de reservation lié à la maison qu'on veut reserver

--- react/src/assets/view/BookVillas/VillaDetailsPage.jsx
+++ react/src/assets/view/BookVillas/VillaDetailsPage.jsx
@@ -1,11 +1,14 @@
 // TestimonialPage.jsx
 import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 import Navbar from "../../components/Navbar.jsx";
 import ImageCard from "../Activities/ImageDisplay.jsx";
+import TextTestimonial from "../Testimoniale/TextTestimonial.jsx";
+import Navigation from "../Testimoniale/Navigation.jsx";
 import Aside from "../../components/Aside.jsx";
 import Footer from "../../components/Footer.jsx";
 import axios from "axios";
+import DetailsVilla from "./DetailsVilla.jsx";
 import ReservationForm from "./ReservationForm.jsx";
 
 function VillabookPage() {
@@ -24,12 +27,22 @@ function VillabookPage() {
     }, []);
 
     const { name } = useParams();
+    const navigate = useNavigate();
     const currentPage = villas.findIndex(villa => villa.Titre === decodeURIComponent(name));
 
     if (currentPage === -1) {
         return <div>Témoignage non trouvé</div>;
     }
 
+    const nextPage = () => {
+        const nextIndex = currentPage < villas.length - 1 ? currentPage + 1 : currentPage;
+        navigate(`/villa/book/${encodeURIComponent(villas[nextIndex].Titre)}`);
+    };
+
+    const prevPage = () => {
+        const prevIndex = currentPage > 0 ? currentPage - 1 : currentPage;
+        navigate(`/villa/book/${encodeURIComponent(villas[prevIndex].Titre)}`);
+    };
 
     const imgPathParts = villas[currentPage].Image.split('/');
     const imgFileName = imgPathParts[imgPathParts.length - 1];
@@ -43,9 +56,23 @@ function VillabookPage() {
 
             <main>
                 <section>
-
+                    <TextTestimonial text={villas[currentPage].Description}/>
+                    <DetailsVilla
+                        key={villas[currentPage].id_villa}
+                        nbAdult={villas[currentPage].Adultes}
+                        nbChildren={villas[currentPage].Enfants}
+                        amenities={villas[currentPage].Equipements}
+                        size={villas[currentPage].Superficie}
+                        categories={villas[currentPage].lieu}
+                        price={villas[currentPage].Prix}
+                    />
                     <ReservationForm />
-
+                    <Navigation
+                        currentPage={currentPage}
+                        totalPages={villas.length}
+                        nextPage={nextPage}
+                        prevPage={prevPage}
+                    />
                 </section>
                 <Aside />
             </main>
