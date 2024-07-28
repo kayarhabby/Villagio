0ebaa6a1021e4ca97edfb816bb181e8ada644bfa
kayarhabby <pasciahershekaya@gmail.com>
2024-07-28 22:45:43 +0200

    suppression de la balise h1

---
+++ react/src/assets/view/BookVillas/SearchRegionTemplate.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import '../../css/template.css'
+import '../../css/RegionTemplate.css'
+export default function SearchRegionTemplate(props) {
+
+    const {
+        src,
+        alt,
+        description,
+        title,
+        nbAdult,
+        nbChildren,
+        amenities,
+        size,
+        categories,
+        price,
+        goToVillaBookPage,
+        goToVillaDetailsPage
+    } = props;
+
+    return (
+        <article>
+            <img src={src} alt={alt}/>
+            <section>
+                <h2>{title}</h2>
+                <p>{description}</p>
+            </section>
+            <section>
+                <ul className="description">
+                    <li>
+                        <span className="title-desc-color">Adults : </span> {nbAdult}
+                    </li>
+                    <li>
+                        <span className="title-desc-color">Children : </span> {nbChildren}
+                    </li>
+                    <li>
+                        <span className="title-desc-color">Amenities :</span> {amenities}
+                    </li>
+                    <li>
+                        <span className="title-desc-color">Size : </span> {size}
+                    </li>
+                    <li>
+                        <span className="title-desc-color">Categories : </span> {categories}
+                    </li>
+                    <li>
+                        <span className="title-desc-color">Prices start at : </span> {price} for 3 nights (+taxes
+                        and fees)
+                    </li>
+                </ul>
+
+                <div className="link">
+                    <button className="btn" onClick={() => goToVillaBookPage(props.title)}>BOOK</button>
+                    <button className="btn" onClick={() => goToVillaDetailsPage(props.title)}>View Details</button>
+                </div>
+            </section>
+        </article>
+    )
+}
