f2fb3509ea2c3b2400e30a4f6aec4c588b283696
kayarhabby <pasciahershekaya@gmail.com>
2024-08-04 11:19:43 +0200

    suppression section selection de langue et menu-hamburger

--- react/src/assets/view/User/NavbarRegistrationForm.jsx
+++ react/src/assets/view/User/NavbarRegistrationForm.jsx
@@ -5,17 +5,10 @@ export default function NavbarRegistrationForm() {
 
     return (
         <nav id="navbar">
-            <section className="menu-hamburger">
-                <a href="#"><i className="fa-solid fa-bars"></i></a>
-                <a href="#"><i className="fa-solid fa-x"></i></a>
-            </section>
-            <section>
-                <a href="/">Villagio</a>
-                <select className="form-select">
-                    <option selected>English</option>
-                    <option value="1">Français</option>
-                </select>
-            </section>
+            <a href="/" style={{
+                fontSize: "2rem",
+                fontWeight: "700"
+            }}>Villagio</a>
         </nav>
     );
 }
