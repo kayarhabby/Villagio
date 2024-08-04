75269d795f6d66ce3979ba80012a3aac2a5bfa0d
kayarhabby <pasciahershekaya@gmail.com>
2024-08-04 11:14:38 +0200

    modification de la structure

--- react/src/assets/view/User/NavbarRegistrationForm.jsx
+++ react/src/assets/view/User/NavbarRegistrationForm.jsx
@@ -5,15 +5,12 @@ export default function NavbarRegistrationForm() {
 
     return (
         <nav id="navbar">
-            <section>
-                <section className="menu-hamburger">
-                    <a href="#"><i className="fa-solid fa-bars"></i></a>
-                    <a href="#"><i className="fa-solid fa-x"></i></a>
-                </section>
-
-                <a href="/">Villagio</a>
+            <section className="menu-hamburger">
+                <a href="#"><i className="fa-solid fa-bars"></i></a>
+                <a href="#"><i className="fa-solid fa-x"></i></a>
             </section>
             <section>
+                <a href="/">Villagio</a>
                 <select className="form-select">
                     <option selected>English</option>
                     <option value="1">Français</option>
