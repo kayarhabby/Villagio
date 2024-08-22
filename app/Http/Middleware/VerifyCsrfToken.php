3395f7fb17e10c26d20aa50ee19ac188392fc6d6
kayarhabby <pasciahershekaya@gmail.com>
2024-08-22 18:42:43 +0200

    update de l'exclusion des routes api de la vérification csrf token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
+        // Laissez ce tableau vide si vous voulez que toutes les routes soient soumises à la vérification CSRF
         'api/*',
     ];
 }
