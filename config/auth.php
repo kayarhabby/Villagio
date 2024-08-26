90b06eb5c85292d2c9fddd1a4936e41c6af003f3
Doumani <doumanisamuel@gmail.com>
2024-08-26 18:14:29 +0000

    Configuration de Auth.php dans config

--- config/auth.php
+++ config/auth.php
@@ -38,7 +38,7 @@ return [
     'guards' => [
         'web' => [
             'driver' => 'session',
-            'provider' => 'users',
+            'provider' => 'client',
         ],
     ],
 
@@ -60,9 +60,9 @@ return [
     */
 
     'providers' => [
-        'users' => [
+        'client' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => App\Models\Client::class,
         ],
 
         // 'users' => [
